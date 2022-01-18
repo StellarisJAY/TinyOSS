eac7b823f37318d21411f51a89f8d61a5a95d651
StellarisJAY <760409558@qq.com>
2022-01-18 14:30:15 +0800

    修复线程安全问题

--- common/src/main/java/com/jay/oss/fs/ChunkManager.java
+++ common/src/main/java/com/jay/oss/fs/ChunkManager.java
@@ -2,6 +2,8 @@ package com.jay.oss.fs;
 
 import com.jay.oss.entity.FileMeta;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.BlockingQueue;
@@ -18,12 +20,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author Jay
  * @date 2022/01/17 15:44
  */
-public class ChunkManager {
+public final class ChunkManager {
     /**
-     * chunkMap
+     * chunkSizeMap
      * key为chunk剩余空间大小，value为该空间大小的所有chunk阻塞队列
      */
-    private final TreeMap<Integer, BlockingQueue<Chunk>> chunkSizeMap = new TreeMap<>();
+    private final SortedMap<Integer, BlockingQueue<Chunk>> chunkSizeMap = Collections.synchronizedSortedMap(new TreeMap<>());
 
     private final ConcurrentHashMap<Integer, Chunk> chunkMap = new ConcurrentHashMap<>();
     /**
@@ -32,7 +34,6 @@ public class ChunkManager {
     private final AtomicInteger chunkIdProvider = new AtomicInteger(1);
 
     private final Object mutex = new Object();
-
     /**
      * 获取一个大小足够容纳FileMeta的chunk
      * @param meta {@link FileMeta}
@@ -41,12 +42,10 @@ public class ChunkManager {
     public Chunk getChunkBySize(FileMeta meta){
         // 获取所有的剩余大小比当前文件大小大的chunk队列
         SortedMap<Integer, BlockingQueue<Chunk>> tailMap = chunkSizeMap.tailMap(meta.getSize());
-        int count = tailMap.size();
-        Chunk chunk = null;
+        Chunk chunk;
         // 遍历有足够空间的chunk
-        for(int i = 0; i < count; i++){
-            BlockingQueue<Chunk> queue = tailMap.get(i);
-            // 尝试从该大小队列中获取一个chunk
+        ArrayList<BlockingQueue<Chunk>> chunkQueues = new ArrayList<>(tailMap.values());
+        for (BlockingQueue<Chunk> queue : chunkQueues) {
             if((chunk = queue.poll()) != null){
                 return chunk;
             }
@@ -60,13 +59,13 @@ public class ChunkManager {
      * @param chunk {@link Chunk}
      */
     public void offerChunk(Chunk chunk){
-        BlockingQueue<Chunk> queue = null;
+        BlockingQueue<Chunk> queue;
         // 保证线程安全，只有一个线程能创建新的queue
-        if((queue = chunkSizeMap.get(chunk.getSize())) == null){
+        if((queue = chunkSizeMap.get(Chunk.MAX_CHUNK_SIZE - chunk.getSize())) == null){
             synchronized (mutex){
                 if((queue = chunkSizeMap.get(chunk.getSize())) == null){
                     queue = new LinkedBlockingQueue<>();
-                    chunkSizeMap.put(chunk.getSize(), queue);
+                    chunkSizeMap.put(Chunk.MAX_CHUNK_SIZE - chunk.getSize(), queue);
                 }
             }
         }
