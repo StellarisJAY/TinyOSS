b4fa62690e5548fccf6bb1f582099f46076cc997
StellarisJAY <760409558@qq.com>
2022-02-12 19:11:43 +0800

    添加meta持久化机制

--- storage/src/main/java/com/jay/oss/storage/StorageNode.java
+++ storage/src/main/java/com/jay/oss/storage/StorageNode.java
@@ -5,46 +5,59 @@ import com.jay.dove.common.AbstractLifeCycle;
 import com.jay.dove.serialize.SerializerManager;
 import com.jay.dove.transport.command.CommandFactory;
 import com.jay.dove.transport.protocol.ProtocolManager;
-import com.jay.dove.util.NamedThreadFactory;
 import com.jay.oss.common.OssConfigs;
 import com.jay.oss.common.fs.ChunkManager;
 import com.jay.oss.common.remoting.FastOssCodec;
 import com.jay.oss.common.remoting.FastOssCommandFactory;
 import com.jay.oss.common.remoting.FastOssProtocol;
 import com.jay.oss.common.serialize.ProtostuffSerializer;
+import com.jay.oss.common.util.Scheduler;
+import com.jay.oss.common.util.ThreadPoolUtil;
 import com.jay.oss.storage.command.StorageNodeCommandHandler;
 import com.jay.oss.storage.meta.MetaManager;
+import com.jay.oss.storage.persistence.Persistence;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *
+ *  StorageNode主类
  * </p>
  *
  * @author Jay
  * @date 2022/01/27 10:39
  */
 public class StorageNode extends AbstractLifeCycle {
+    /**
+     * TCP 服务器
+     */
     private final DoveServer server;
+    /**
+     * meta管理器
+     */
     private final MetaManager metaManager;
+    /**
+     * chunk管理器
+     */
     private final ChunkManager chunkManager;
+    /**
+     * 命令处理器
+     */
     private final StorageNodeCommandHandler commandHandler;
+    /**
+     * 持久化工具
+     */
+    private final Persistence persistence;
 
     public StorageNode(int port) {
         CommandFactory commandFactory = new FastOssCommandFactory();
         this.metaManager = new MetaManager();
         this.chunkManager = new ChunkManager();
+        this.persistence = new Persistence(metaManager, chunkManager);
         // commandHandler执行器线程池
-        ExecutorService commandHandlerExecutor = new ThreadPoolExecutor(2 * Runtime.getRuntime().availableProcessors(),
-                2 * Runtime.getRuntime().availableProcessors(),
-                0, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(),
-                new NamedThreadFactory("command-handler-thread-", true));
-        // StorageNodeCommandHandler
+        ExecutorService commandHandlerExecutor = ThreadPoolUtil.newIoThreadPool("command-handler-worker-");
+        // 命令处理器
         this.commandHandler = new StorageNodeCommandHandler(commandFactory, commandHandlerExecutor, chunkManager, metaManager);
         // FastOSS协议Dove服务器
         this.server = new DoveServer(new FastOssCodec(), port, commandFactory);
@@ -55,6 +68,10 @@ public class StorageNode extends AbstractLifeCycle {
         ProtocolManager.registerProtocol(FastOssProtocol.PROTOCOL_CODE, new FastOssProtocol(commandHandler));
         // 注册序列化器
         SerializerManager.registerSerializer(OssConfigs.PROTOSTUFF_SERIALIZER, new ProtostuffSerializer());
+        // 加载持久化数据
+        persistence.loadMeta();
+        // 提交持久化定时任务
+        Scheduler.scheduleAtFixedRate(persistence::persistenceMeta, 30, 30, TimeUnit.SECONDS);
     }
 
 
---
+++ storage/src/main/java/com/jay/oss/storage/persistence/Persistence.java
@@ -0,0 +1,129 @@
+package com.jay.oss.storage.persistence;
+
+import com.jay.oss.common.entity.FileMetaWithChunkInfo;
+import com.jay.oss.common.fs.ChunkManager;
+import com.jay.oss.common.fs.FileChunkIndex;
+import com.jay.oss.storage.meta.MetaManager;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * <p>
+ *  持久化工具
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/12 15:23
+ */
+@Slf4j
+public class Persistence {
+
+    private final MetaManager metaManager;
+    private final ChunkManager chunkManager;
+
+    public Persistence(MetaManager metaManager, ChunkManager chunkManager) {
+        this.metaManager = metaManager;
+        this.chunkManager = chunkManager;
+    }
+
+    /**
+     * meta数据持久化
+     * 持久化格式：
+     * +----------+-------+--------+---------+--------+--------+-------+----------+-----------+
+     * |  keyLen  |  key  |  fLen  |  fName  |  size  |  time  |  cId  |  offset  |  removed  |
+     * +----------+-------+--------+---------+--------+--------+-------+----------+-----------+
+     * keyLen：key长度，2bytes
+     * key：keyLen bytes
+     * fLen：filename长度，2bytes
+     * fName：fLen bytes
+     * size：文件大小 8 bytes
+     * time：创建时间，8 bytes
+     * cId：chunk id，4 bytes
+     * offset：chunk偏移，4 bytes
+     * removed：删除标记，1 byte
+     * 总长度：keyLen + fLen + 29 bytes
+     */
+    public void persistenceMeta(){
+        File file = new File("C:/Users/76040/Documents/oss/meta/objects.data");
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+            }catch (Exception e){
+                log.error("can't create persistence file for metas");
+                return;
+            }
+        }
+        try(FileOutputStream outputStream = new FileOutputStream(file);
+            FileChannel fileChannel = outputStream.getChannel();){
+            long start = System.nanoTime();
+            List<FileMetaWithChunkInfo> snapshot = metaManager.snapshot();
+            for (FileMetaWithChunkInfo meta : snapshot) {
+                byte[] key = meta.getKey().getBytes(StandardCharsets.UTF_8);
+                byte[] filename = meta.getFilename().getBytes(StandardCharsets.UTF_8);
+                // 分配buffer，长度
+                ByteBuffer buffer = ByteBuffer.allocate(key.length + filename.length + 29);
+                // 文件key长度用short表示足够了，可以节省2个字节
+                buffer.putShort((short)key.length);
+                buffer.put(key);
+                buffer.putShort((short)filename.length);
+                buffer.put(filename);
+                buffer.putLong(meta.getSize());
+                buffer.putLong(meta.getCreateTime());
+                buffer.putInt(meta.getChunkIndex().getChunkId());
+                buffer.putInt(meta.getChunkIndex().getOffset());
+                buffer.put(meta.getChunkIndex().isRemoved() ? (byte)1 : (byte)0);
+                buffer.rewind();
+                fileChannel.write(buffer);
+            }
+            log.info("object meta saved, time used: {}", (System.nanoTime() - start));
+        }catch (IOException e){
+            log.error("save meta persistence error ", e);
+        }
+    }
+
+    /**
+     * 加载meta persistence
+     */
+    public void loadMeta(){
+        File file = new File("C:/Users/76040/Documents/oss/meta/objects.data");
+        try(FileInputStream inputStream = new FileInputStream(file);
+            FileChannel fileChannel = inputStream.getChannel();){
+            long start = System.nanoTime();
+            int count = 0;
+            MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            while(buffer.hasRemaining()){
+                short keyLen = buffer.getShort();
+                byte[] keyBytes = new byte[keyLen];
+                buffer.get(keyBytes);
+                short fLen = buffer.getShort();
+                byte[] fileName = new byte[fLen];
+                buffer.get(fileName);
+                long size = buffer.getLong();
+                long createTime = buffer.getLong();
+                int chunkId = buffer.getInt();
+                int offset = buffer.getInt();
+                byte removed = buffer.get();
+
+                FileChunkIndex chunkIndex = new FileChunkIndex(chunkId, offset, size, removed == 1);
+                String key = new String(keyBytes, StandardCharsets.UTF_8);
+                FileMetaWithChunkInfo meta = FileMetaWithChunkInfo.builder()
+                        .key(key).filename(new String(fileName, StandardCharsets.UTF_8))
+                        .createTime(createTime).size(size)
+                        .chunkIndex(chunkIndex).build();
+                metaManager.saveMeta(meta);
+                count++;
+            }
+            log.info("load meta finished, loaded: {}, time used: {}", count, (System.nanoTime() - start));
+        }catch (FileNotFoundException e){
+            log.info("no meta persistence found, skipping loading meta");
+        }catch (IOException e){
+            log.error("loading meta persistence error ", e);
+        }
+    }
+}
