55a6280bd321bcbe68e7e3e31b551bbf1900db36
StellarisJAY <760409558@qq.com>
2022-02-12 11:42:56 +0800

    添加StorageNode下载处理器

--- storage/src/main/java/com/jay/oss/storage/command/StorageNodeCommandHandler.java
+++ storage/src/main/java/com/jay/oss/storage/command/StorageNodeCommandHandler.java
@@ -5,6 +5,7 @@ import com.jay.oss.common.fs.ChunkManager;
 import com.jay.oss.common.remoting.FastOssCommandHandler;
 import com.jay.oss.common.remoting.FastOssProtocol;
 import com.jay.oss.storage.meta.MetaManager;
+import com.jay.oss.storage.processor.FileDownloadProcessor;
 import com.jay.oss.storage.processor.FileUploadProcessor;
 
 import java.util.concurrent.ExecutorService;
@@ -24,7 +25,10 @@ public class StorageNodeCommandHandler extends FastOssCommandHandler {
         super(commandFactory, executor);
         // 文件上传处理器
         FileUploadProcessor fileUploadProcessor = new FileUploadProcessor(chunkManager, metaManager, commandFactory);
+        FileDownloadProcessor fileDownloadProcessor = new FileDownloadProcessor(metaManager, chunkManager, commandFactory);
         this.registerProcessor(FastOssProtocol.UPLOAD_FILE_HEADER, fileUploadProcessor);
         this.registerProcessor(FastOssProtocol.UPLOAD_FILE_PARTS, fileUploadProcessor);
+        this.registerProcessor(FastOssProtocol.DOWNLOAD_FULL, fileDownloadProcessor);
+        this.registerProcessor(FastOssProtocol.DOWNLOAD_RANGED, fileDownloadProcessor);
     }
 }
