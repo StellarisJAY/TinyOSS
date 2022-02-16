c37b325f212ef31ff02e4a8a1c924da2dedbe2de
StellarisJAY <760409558@qq.com>
2022-02-16 16:15:22 +0800

    添加ListBucket请求实体

---
+++ common/src/main/java/com/jay/oss/common/entity/ListBucketRequest.java
@@ -0,0 +1,38 @@
+package com.jay.oss.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ *  List Bucket请求
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/16 14:21
+ */
+@AllArgsConstructor
+@Getter
+@Setter
+public class ListBucketRequest {
+    /**
+     * 桶名称
+     */
+    private String bucket;
+
+    /**
+     * 访问token
+     */
+    private String token;
+
+    /**
+     * list数量
+     */
+    private int count;
+
+    /**
+     * 偏移量
+     */
+    private int offset;
+}
