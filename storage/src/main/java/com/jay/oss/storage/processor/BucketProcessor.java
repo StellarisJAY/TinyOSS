7c0547e841870cde6361cfd3c8d990c685a32e74
StellarisJAY <760409558@qq.com>
2022-02-16 16:15:06 +0800

    添加Storage List Bucket处理器

--- storage/src/main/java/com/jay/oss/storage/processor/BucketProcessor.java
+++ storage/src/main/java/com/jay/oss/storage/processor/BucketProcessor.java
@@ -1,14 +1,21 @@
 package com.jay.oss.storage.processor;
 
+import com.alibaba.fastjson.JSON;
 import com.jay.dove.transport.command.AbstractProcessor;
 import com.jay.dove.transport.command.CommandCode;
 import com.jay.dove.transport.command.CommandFactory;
+import com.jay.oss.common.acl.Acl;
 import com.jay.oss.common.entity.Bucket;
+import com.jay.oss.common.entity.FileMeta;
+import com.jay.oss.common.entity.ListBucketRequest;
 import com.jay.oss.common.remoting.FastOssCommand;
 import com.jay.oss.common.remoting.FastOssProtocol;
+import com.jay.oss.common.util.AccessTokenUtil;
 import com.jay.oss.common.util.SerializeUtil;
 import com.jay.oss.storage.meta.BucketManager;
 import io.netty.channel.ChannelHandlerContext;
+
+import java.util.List;
 
 /**
  * <p>
@@ -36,6 +43,9 @@ public class BucketProcessor extends AbstractProcessor {
             if(FastOssProtocol.PUT_BUCKET.equals(code)){
                 processPutBucket(context, command);
             }
+            else if(FastOssProtocol.LIST_BUCKET.equals(code)){
+                processListBucket(context, command);
+            }
         }
     }
 
@@ -52,4 +62,41 @@ public class BucketProcessor extends AbstractProcessor {
                 .createResponse(command.getId(), keyPair, FastOssProtocol.SUCCESS);
         sendResponse(context, response);
     }
+
+    /**
+     * 处理list bucket请求
+     * @param context {@link ChannelHandlerContext}
+     * @param command {@link FastOssCommand}
+     */
+    private void processListBucket(ChannelHandlerContext context, FastOssCommand command){
+        byte[] content = command.getContent();
+        // 反序列化请求
+        ListBucketRequest request = SerializeUtil.deserialize(content, ListBucketRequest.class);
+        // 获取bucket
+        Bucket bucket = bucketManager.getBucket(request.getBucket());
+
+        FastOssCommand response;
+        // bucket 不存在
+        if(bucket == null){
+            response = (FastOssCommand)commandFactory
+                    .createResponse(command.getId(), "bucket not found", FastOssProtocol.NOT_FOUND);
+        }else{
+            // 检查ACL，
+            Acl acl = bucket.getAcl();
+            // 如果访问权限是 PRIVATE 且 token无效，拒绝访问
+            if(acl == Acl.PRIVATE && !AccessTokenUtil.checkAccessToken(bucket.getAccessKey(), bucket.getSecretKey(), request.getToken())){
+                response = (FastOssCommand) commandFactory
+                        .createResponse(command.getId(), "invalid access token", FastOssProtocol.ACCESS_DENIED);
+            }else{
+                // list bucket
+                List<FileMeta> objects = bucketManager.listBucket(request.getBucket(), request.getCount(), request.getOffset());
+                // 转换成JSON
+                String json = JSON.toJSONString(objects);
+                response = (FastOssCommand)commandFactory
+                        .createResponse(command.getId(), json, FastOssProtocol.SUCCESS);
+            }
+        }
+        sendResponse(context, response);
+    }
+
 }
