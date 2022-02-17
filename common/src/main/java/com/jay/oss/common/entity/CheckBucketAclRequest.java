05f85f6dba0ad2a98132ee278d46fdf98e231b30
StellarisJAY <760409558@qq.com>
2022-02-17 11:41:50 +0800

    添加桶权限校验请求实体

---
+++ common/src/main/java/com/jay/oss/common/entity/CheckBucketAclRequest.java
@@ -0,0 +1,32 @@
+package com.jay.oss.common.entity;
+
+import com.jay.oss.common.acl.BucketAccessMode;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * <p>
+ *  检查访问权限请求
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/17 10:57
+ */
+@Builder
+@Getter
+public class CheckBucketAclRequest {
+    /**
+     * 桶，bucketName-AppId
+     */
+    private String bucket;
+
+    /**
+     * 访问Token
+     */
+    private String token;
+
+    /**
+     * 访问模式，读、写、写权限
+     */
+    private BucketAccessMode accessMode;
+}
