63a61c65f953bd3d09e2573811c8dca25912d7d0
StellarisJAY <760409558@qq.com>
2022-02-21 13:22:06 +0800

    添加存储节点选择策略

---
+++ tracker/src/main/java/com/jay/oss/tracker/meta/AbstractStorageSelector.java
@@ -0,0 +1,38 @@
+package com.jay.oss.tracker.meta;
+
+import com.jay.oss.common.registry.StorageNodeInfo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/21 11:43
+ */
+public abstract class AbstractStorageSelector implements StorageSelector{
+    @Override
+    public List<StorageNodeInfo> select(List<StorageNodeInfo> nodes, long size, int replica) {
+        // 按大小升序排序，优先选择剩余空间较多的storage
+        List<StorageNodeInfo> selections = nodes.stream().sorted((o1, o2) -> (int) (o1.getSpace() - o2.getSpace()))
+                .filter((nodeInfo) -> nodeInfo.getSpace() > size)
+                .collect(Collectors.toList());
+        // 节点数量是否足够备份
+        if(selections.size() <= replica){
+            return selections;
+        }
+        // 执行选择策略
+        return doSelect(selections, size, replica);
+    }
+
+    /**
+     * 选择策略
+     * @param nodes 存储节点
+     * @param size 目标大小
+     * @return 存储节点
+     */
+    public abstract List<StorageNodeInfo> doSelect(List<StorageNodeInfo> nodes, long size, int replica);
+}
