0797fc630efd703137938d9c95883bc9d466ae54
StellarisJAY <760409558@qq.com>
2022-02-24 14:51:35 +0800

    添加默认副本数量配置

--- common/src/main/java/com/jay/oss/common/config/OssConfigs.java
+++ common/src/main/java/com/jay/oss/common/config/OssConfigs.java
@@ -19,21 +19,32 @@ public class OssConfigs {
      * 默认序列化器
      */
     public static final byte DEFAULT_SERIALIZER = PROTOSTUFF_SERIALIZER;
-
+    /**
+     * Zookeeper 地址
+     */
     private static final String ZOOKEEPER_REGISTRY_HOST = "oss.registry.zookeeper.host";
     public static final int ZOOKEEPER_SESSION_TIMEOUT = 3 * 1000;
 
+    /**
+     * 数据目录
+     */
     private static final String DATA_PATH = "oss.data.path";
     public static final String DEFAULT_DATA_PATH = "./data";
 
-    private static final String GROUP = "oss.storage.group";
-
-    private static final String FLUSH_INTERVAL_LOGS = "oss.edit.flush-interval-log";
-
+    /**
+     * Tracker服务器 地址
+     */
     private static final String TRACKER_SERVER = "oss.tracker.host";
 
+    /**
+     * 副本数量
+     */
     private static final String REPLICA = "oss.replica.count";
+    private static final int DEFAULT_REPLICA_COUNT = 3;
 
+    /**
+     * 一致性hash环，虚节点数量
+     */
     private static final String VNODE_COUNT = "oss.load-balance.vnode";
     private static final int DEFAULT_VNODE_COUNT = 10;
 
@@ -55,7 +66,7 @@ public class OssConfigs {
     }
 
     public static int replicaCount(){
-        return ConfigsManager.getInt(REPLICA, 1);
+        return ConfigsManager.getInt(REPLICA, DEFAULT_REPLICA_COUNT);
     }
 
     public static int vnodeCount(){
