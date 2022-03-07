590c0e68e757a9c5a67c36f5434ed25c046e97ab
StellarisJAY <760409558@qq.com>
2022-03-07 16:56:07 +0800

    添加Multipart上传查询请求

---
+++ common/src/main/java/com/jay/oss/common/entity/LookupMultipartUploadRequest.java
@@ -0,0 +1,25 @@
+package com.jay.oss.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/03/07 15:37
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class LookupMultipartUploadRequest {
+    private String uploadId;
+    private String objectKey;
+    private String bucket;
+    private String token;
+}
