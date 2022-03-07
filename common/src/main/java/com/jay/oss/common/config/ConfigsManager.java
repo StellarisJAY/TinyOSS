a97900466a8012dc644b27fd0c0f1ee4b29b1d19
StellarisJAY <760409558@qq.com>
2022-03-07 13:30:30 +0800

    添加自定义配置文件路径

--- common/src/main/java/com/jay/oss/common/config/ConfigsManager.java
+++ common/src/main/java/com/jay/oss/common/config/ConfigsManager.java
@@ -20,21 +20,25 @@ import java.util.Properties;
 public class ConfigsManager {
     private static Properties properties = new Properties();
 
-    public static void loadConfigs() {
+    public static void loadConfigs(){
+        loadConfigs("fast-oss.conf");
+    }
+
+    public static void loadConfigs(String configFileName) {
         String path = ConfigsManager.class.getClassLoader().getResource("com/jay/oss/common/config/ConfigsManager.class").getPath();
         int i = path.indexOf("!");
         if(i != -1){
             String jarPath = path.substring(0, i);
             String dir = jarPath.substring(6, jarPath.lastIndexOf("/"));
-            File file = new File(dir + "/conf/fast-oss.conf");
+            File file = new File(dir + "/conf/" + configFileName);
             try(InputStream inputStream = new FileInputStream(file)){
                 properties.load(inputStream);
             }catch (IOException e){
-                log.error("failed to load configs from {}", "fast-oss.conf", e);
+                log.error("failed to load configs from {}", configFileName, e);
                 throw new RuntimeException(e);
             }
         }else{
-            try(InputStream stream = ConfigsManager.class.getClassLoader().getResourceAsStream("fast-oss.conf")){
+            try(InputStream stream = ConfigsManager.class.getClassLoader().getResourceAsStream(configFileName)){
                 properties.load(stream);
             }catch (Exception e){
                 log.error("failed to load configs from {}", "fast-oss.conf", e);
