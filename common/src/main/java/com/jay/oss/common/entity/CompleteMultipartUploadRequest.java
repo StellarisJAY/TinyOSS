d4e719876763f319a1d50abc910d12e53adf0958
StellarisJAY <760409558@qq.com>
2022-03-09 16:41:26 +0800

    添加Complete分片上传请求实体

---
+++ common/src/main/java/com/jay/oss/common/entity/CompleteMultipartUploadRequest.java
@@ -0,0 +1,29 @@
+package com.jay.oss.common.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *  完成分片上传请求
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/03/09 15:52
+ */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CompleteMultipartUploadRequest implements Serializable {
+    private String uploadId;
+    private String objectKey;
+    private String bucket;
+    private String token;
+
+    private String filename;
+    private long size;
+}
