756c59d7bcd0795f66d4abdbfe6fabd15dd6cb3d
StellarisJAY <760409558@qq.com>
2022-03-11 11:39:27 +0800

    添加Http请求常量

--- proxy/src/main/java/com/jay/oss/proxy/constant/HttpConstants.java
+++ proxy/src/main/java/com/jay/oss/proxy/constant/HttpConstants.java
@@ -2,7 +2,7 @@ package com.jay.oss.proxy.constant;
 
 /**
  * <p>
- *
+ *  Http常量
  * </p>
  *
  * @author Jay
@@ -13,4 +13,14 @@ public class HttpConstants {
     public static final String UPLOAD_ID = "uploadId";
     public static final String UPLOAD_PART_NUM = "partNum";
     public static final String VERSION_ID = "versionId";
+
+    public static final String HOST = "Host";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String CONTENT_MD5 = "Content-MD5";
+
+    public static final String RANGE = "Range";
+    public static final String AUTHORIZATION = "Authorization";
+
+    public static final String ACL = "foss-acl";
 }
