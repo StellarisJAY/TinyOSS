3e60992e51072559fdd6d46afe287f94afce15a0
StellarisJAY <760409558@qq.com>
2022-03-19 11:00:00 +0800

    更新Storage节点信息

--- common/src/main/java/com/jay/oss/common/util/NodeInfoCollector.java
+++ common/src/main/java/com/jay/oss/common/util/NodeInfoCollector.java
@@ -5,6 +5,7 @@ import com.jay.oss.common.registry.StorageNodeInfo;
 
 import java.io.File;
 import java.net.Inet4Address;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * <p>
@@ -14,13 +15,30 @@ import java.net.Inet4Address;
  * @author Jay
  * @date 2022/02/21 10:49
  */
-public class NodeInfoUtil {
+public class NodeInfoCollector {
+
+    private static final AtomicLong IO_COUNTER = new AtomicLong(0);
+    private static long lastIoCount = 0;
+
     public static StorageNodeInfo getStorageNodeInfo(int port) throws Exception {
         String path = OssConfigs.dataPath();
         File dir = new File(path);
+
+        long ioCount = IO_COUNTER.get();
+        long ioRate = ioCount - lastIoCount;
+        lastIoCount = ioCount;
         return StorageNodeInfo.builder()
                 .url(Inet4Address.getLocalHost().getHostAddress() + ":" + port)
                 .space(dir.getUsableSpace())
+                .memoryTotal(Runtime.getRuntime().totalMemory())
+                .ioRate(ioRate)
                 .available(true).build();
+    }
+
+    /**
+     * 记录IO次数
+     */
+    public static void incrementIoCount(){
+        IO_COUNTER.incrementAndGet();
     }
 }
