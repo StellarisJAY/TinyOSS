b97d603e54fa2f44829143028f3343257e02358a
StellarisJAY <760409558@qq.com>
2022-03-22 11:19:06 +0800

    common添加消息生产者消费者

---
+++ common/src/main/java/com/jay/oss/common/kafka/RecordProducer.java
@@ -0,0 +1,53 @@
+package com.jay.oss.common.kafka;
+
+import com.jay.oss.common.config.OssConfigs;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+/**
+ * <p>
+ *  消息生产者
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/03/21 13:38
+ */
+@Slf4j
+public class RecordProducer {
+    private final KafkaProducer<String, String> producer;
+
+    public RecordProducer(){
+        this.producer = new KafkaProducer<>(getProperties(), new StringSerializer(), new StringSerializer());
+    }
+
+    private Properties getProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, OssConfigs.kafkaServers());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, OssConfigs.kafkaAcks());
+        return properties;
+    }
+
+    /**
+     * 发送消息到目标主题
+     * @param topic 主题
+     * @param key key
+     * @param value value
+     * @return {@link Future < RecordMetadata >} 异步结果
+     */
+    public final Future<RecordMetadata> send(String topic, String key, String value){
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+        return producer.send(record, (metadata, exception) -> {
+            if(exception != null){
+                // 消息发送错误，等待重发
+                log.error("Producer send record failed, topic: {}, key: {}", topic, key, exception);
+            }
+        });
+    }
+}
