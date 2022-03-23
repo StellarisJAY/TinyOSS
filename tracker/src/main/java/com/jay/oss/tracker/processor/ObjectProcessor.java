b489ae598734fae6d24e7a241dfb056088d44718
StellarisJAY <760409558@qq.com>
2022-03-23 16:44:03 +0800

    Tracker端请求处理业务与存储桶权限校验解耦

--- tracker/src/main/java/com/jay/oss/tracker/processor/ObjectProcessor.java
+++ tracker/src/main/java/com/jay/oss/tracker/processor/ObjectProcessor.java
@@ -1,10 +1,8 @@
 package com.jay.oss.tracker.processor;
 
-import com.jay.dove.transport.command.AbstractProcessor;
 import com.jay.dove.transport.command.CommandCode;
 import com.jay.dove.transport.command.CommandFactory;
 import com.jay.dove.transport.command.RemotingCommand;
-import com.jay.oss.common.acl.BucketAccessMode;
 import com.jay.oss.common.constant.OssConstants;
 import com.jay.oss.common.edit.EditLog;
 import com.jay.oss.common.edit.EditLogManager;
@@ -12,6 +10,7 @@ import com.jay.oss.common.edit.EditOperation;
 import com.jay.oss.common.entity.DeleteObjectInBucketRequest;
 import com.jay.oss.common.entity.LocateObjectRequest;
 import com.jay.oss.common.entity.object.ObjectMeta;
+import com.jay.oss.common.entity.object.ObjectVO;
 import com.jay.oss.common.kafka.RecordProducer;
 import com.jay.oss.common.remoting.FastOssCommand;
 import com.jay.oss.common.remoting.FastOssProtocol;
@@ -19,12 +18,7 @@ import com.jay.oss.common.util.SerializeUtil;
 import com.jay.oss.common.util.StringUtil;
 import com.jay.oss.tracker.meta.BucketManager;
 import com.jay.oss.tracker.track.ObjectTracker;
-import com.jay.oss.tracker.util.BucketAclUtil;
-import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.RecordMetadata;
-
-import java.util.concurrent.Future;
 
 /**
  * <p>
@@ -36,130 +30,114 @@ import java.util.concurrent.Future;
  * @date 2022/03/02 12:07
  */
 @Slf4j
-public class ObjectProcessor extends AbstractProcessor {
-    private final BucketManager bucketManager;
+public class ObjectProcessor extends TrackerProcessor {
     private final ObjectTracker objectTracker;
     private final EditLogManager editLogManager;
     private final RecordProducer trackerProducer;
-    private final CommandFactory commandFactory;
-    //private final KafkaProducer<String, String> deleteMessageProducer;
 
     public ObjectProcessor(BucketManager bucketManager, ObjectTracker objectTracker, EditLogManager editLogManager, RecordProducer trackerProducer, CommandFactory commandFactory) {
-        this.bucketManager = bucketManager;
+        super(commandFactory, bucketManager);
         this.objectTracker = objectTracker;
         this.editLogManager = editLogManager;
         this.trackerProducer = trackerProducer;
-        this.commandFactory = commandFactory;
-        //this.deleteMessageProducer = new KafkaProducer<>(OssConfigs.getProperties(), new StringSerializer(), new StringSerializer());
     }
 
     @Override
-    public void process(ChannelHandlerContext channelHandlerContext, Object o) {
-        if(o instanceof FastOssCommand){
-            FastOssCommand command = (FastOssCommand) o;
-            CommandCode code = command.getCommandCode();
-            if(FastOssProtocol.LOCATE_OBJECT.equals(code)){
-                locateObject(channelHandlerContext, command);
-            }
-            else if(FastOssProtocol.DELETE_OBJECT.equals(code)){
-                deleteObject(channelHandlerContext, command);
-            }
+    public RemotingCommand doProcess(FastOssCommand command) {
+        CommandCode code = command.getCommandCode();
+        if(FastOssProtocol.LOCATE_OBJECT.equals(code)){
+            return locateObject(command);
         }
+        else if(FastOssProtocol.DELETE_OBJECT.equals(code)){
+            return deleteObject(command);
+        }
+        else if(FastOssProtocol.GET_OBJECT_META.equals(code)){
+            return getObjectMeta(command);
+        }
+        return commandFactory.createResponse(command.getId(), "", FastOssProtocol.ERROR);
     }
 
     /**
      * 获取object位置，同时检查存储桶访问权限
-     * @param context {@link ChannelHandlerContext}
      * @param command {@link FastOssCommand}
+     * @return {@link RemotingCommand}
      */
-    private void locateObject(ChannelHandlerContext context, FastOssCommand command){
+    private RemotingCommand locateObject(FastOssCommand command){
         byte[] content = command.getContent();
         LocateObjectRequest request = SerializeUtil.deserialize(content, LocateObjectRequest.class);
-        String bucket = request.getBucket();
-        String token = request.getToken();
         String objectKey = request.getObjectKey();
-        RemotingCommand response;
-        // 检查存储桶访问权限
-        CommandCode code = BucketAclUtil.checkAuthorization(bucketManager, bucket, token, BucketAccessMode.READ);
-        if(FastOssProtocol.SUCCESS.equals(code)){
-            // 定位object
-            String urls = objectTracker.locateObject(objectKey);
-            if(StringUtil.isNullOrEmpty(urls)){
-                response = commandFactory.createResponse(command.getId(), "", FastOssProtocol.OBJECT_NOT_FOUND);
-            }else{
-                response = commandFactory.createResponse(command.getId(), urls, FastOssProtocol.SUCCESS);
-            }
+        // 定位object
+        String urls = objectTracker.locateObject(objectKey);
+        if(StringUtil.isNullOrEmpty(urls)){
+            return commandFactory.createResponse(command.getId(), "", FastOssProtocol.OBJECT_NOT_FOUND);
         }else{
-            response = commandFactory.createResponse(command.getId(), "", code);
+            return commandFactory.createResponse(command.getId(), urls, FastOssProtocol.SUCCESS);
         }
-        sendResponse(context, response);
     }
 
     /**
      * 删除object
-     * @param context {@link ChannelHandlerContext}
      * @param command {@link FastOssCommand}
+     * @return {@link RemotingCommand}
      */
-    private void deleteObject(ChannelHandlerContext context, FastOssCommand command){
+    private RemotingCommand deleteObject(FastOssCommand command){
         DeleteObjectInBucketRequest request = SerializeUtil.deserialize(command.getContent(), DeleteObjectInBucketRequest.class);
-        String bucket = request.getBucket();
         String objectKey = request.getObjectKey();
-        String token = request.getToken();
-        // 检查存储桶权限
-        CommandCode code = BucketAclUtil.checkAuthorization(bucketManager, bucket, token, BucketAccessMode.WRITE);
-        RemotingCommand response;
-        if(FastOssProtocol.SUCCESS.equals(code)){
-            // 定位并删除object
-            String urls = objectTracker.locateAndDeleteObject(objectKey);
-            if(StringUtil.isNullOrEmpty(urls)){
-                response = commandFactory.createResponse(command.getId(), "", FastOssProtocol.OBJECT_NOT_FOUND);
-            }
-            else{
-                bucketManager.deleteObject(bucket, objectKey);
-                // editLog记录删除操作
-                appendDeleteObjectLog(objectKey);
-                // 发送删除object消息，由Storage收到消息后异步删除object数据
-                Future<RecordMetadata> future = trackerProducer.send(OssConstants.DELETE_OBJECT_TOPIC, objectKey, objectKey);
-                response = commandFactory.createResponse(command.getId(), "", FastOssProtocol.SUCCESS);
-            }
+        String bucket = request.getBucket();
+        // 定位并删除object
+        String urls = objectTracker.locateAndDeleteObject(objectKey);
+        if(StringUtil.isNullOrEmpty(urls)){
+            return commandFactory.createResponse(command.getId(), "", FastOssProtocol.OBJECT_NOT_FOUND);
         }
         else{
-            response = commandFactory.createResponse(command.getId(), "", code);
+            bucketManager.deleteObject(bucket, objectKey);
+            // editLog记录删除操作
+            appendDeleteObjectLog(objectKey);
+            // 发送删除object消息，由Storage收到消息后异步删除object数据
+            trackerProducer.send(OssConstants.DELETE_OBJECT_TOPIC, objectKey, objectKey);
+            return commandFactory.createResponse(command.getId(), "", FastOssProtocol.SUCCESS);
         }
-        sendResponse(context, response);
     }
 
 
     /**
      * 获取object元数据
-     * @param context {@link ChannelHandlerContext}
      * @param command {@link FastOssCommand}
+     * @return {@link RemotingCommand}
      */
-    private void getObjectMeta(ChannelHandlerContext context, FastOssCommand command){
+    private RemotingCommand getObjectMeta(FastOssCommand command){
         LocateObjectRequest request = SerializeUtil.deserialize(command.getContent(), LocateObjectRequest.class);
-        String bucket = request.getBucket();
         String objectKey = request.getObjectKey();
-        String token = request.getToken();
-        CommandCode code = BucketAclUtil.checkAuthorization(bucketManager, bucket, token, BucketAccessMode.READ);
-        RemotingCommand response;
-        if(FastOssProtocol.SUCCESS.equals(code)){
-            ObjectMeta objectMeta = objectTracker.getObjectMeta(objectKey);
-            if(objectMeta == null){
-                response = commandFactory.createResponse(command.getId(), "", FastOssProtocol.OBJECT_NOT_FOUND);
-            }else{
-                byte[] content = SerializeUtil.serialize(objectMeta, ObjectMeta.class);
-                response = commandFactory.createResponse(command.getId(), content, code);
-            }
+        ObjectMeta objectMeta = objectTracker.getObjectMeta(objectKey);
+        if(objectMeta == null){
+            return commandFactory.createResponse(command.getId(), "", FastOssProtocol.OBJECT_NOT_FOUND);
         }else{
-            response = commandFactory.createResponse(command.getId(), "", code);
+            byte[] content = SerializeUtil.serialize(getObjectVO(objectMeta), ObjectVO.class);
+            return commandFactory.createResponse(command.getId(), content, FastOssProtocol.SUCCESS);
         }
-        sendResponse(context, response);
     }
 
 
-
+    /**
+     * 追加删除object日志
+     * @param objectKey objectKey
+     */
     private void appendDeleteObjectLog(String objectKey){
         EditLog editLog = new EditLog(EditOperation.BUCKET_DELETE_OBJECT, StringUtil.getBytes(objectKey));
         editLogManager.append(editLog);
+    }
+
+    /**
+     * object元数据转换成VO
+     * @param meta {@link ObjectMeta}
+     * @return {@link ObjectVO}
+     */
+    private ObjectVO getObjectVO(ObjectMeta meta){
+        return ObjectVO.builder()
+                .objectKey(meta.getObjectKey()).fileName(meta.getFileName())
+                .createTime(meta.getCreateTime()).md5(meta.getMd5())
+                .versionId(meta.getVersionId())
+                .size(meta.getSize())
+                .build();
     }
 }
