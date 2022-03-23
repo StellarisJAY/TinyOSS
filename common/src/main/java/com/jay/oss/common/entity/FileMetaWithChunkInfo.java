f5756a7cb539c05f5d899eb0e5f509e983268dc5
StellarisJAY <760409558@qq.com>
2022-03-23 10:45:18 +0800

    Storage端的对象元数据添加标记删除属性

--- common/src/main/java/com/jay/oss/common/entity/FileMetaWithChunkInfo.java
+++ common/src/main/java/com/jay/oss/common/entity/FileMetaWithChunkInfo.java
@@ -26,12 +26,23 @@ public class FileMetaWithChunkInfo {
      * 该文件数据的offset
      */
     private int offset;
+
+    /**
+     * 文件被标记删除
+     * 该标记表示文件已无法访问，但是数据还未被彻底删除
+     */
+    private boolean markRemoved;
     /**
      * 文件已被删除
+     * 如果该标记为true，表示文件已经被彻底删除
      */
     private boolean removed;
 
     public void setRemoved(boolean removed){
         this.removed = removed;
+    }
+
+    public void setMarkRemoved(boolean removed){
+        markRemoved = removed;
     }
 }
