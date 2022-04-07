194f50a9b0493b01106a1b5ee5e7a71f824bcbdb
StellarisJAY <760409558@qq.com>
2022-04-07 17:24:42 +0800

    更新BitCask存储模型，添加启动时加载索引

--- common/src/main/java/com/jay/oss/common/bitcask/BitCaskStorage.java
+++ common/src/main/java/com/jay/oss/common/bitcask/BitCaskStorage.java
@@ -3,14 +3,13 @@ package com.jay.oss.common.bitcask;
 import com.jay.oss.common.config.OssConfigs;
 import com.jay.oss.common.util.CompressUtil;
 import com.jay.oss.common.util.StringUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -40,10 +39,6 @@ public class BitCaskStorage {
      */
     private Chunk activeChunk = null;
 
-    /**
-     * Hint 日志
-     */
-    private HintLog hintLog;
 
     private static final byte[] DELETE_TAG = new byte[]{(byte)127};
 
@@ -81,9 +76,7 @@ public class BitCaskStorage {
             }
             chunks.sort(Comparator.comparingInt(Chunk::getChunkId));
         }
-        // 加载索引日志
-        hintLog = new HintLog();
-        hintLog.init();
+        loadIndex();
         log.info("BitCask Storage loaded {} chunks", chunks.size());
     }
 
@@ -100,7 +93,7 @@ public class BitCaskStorage {
      * @throws IOException e
      */
     private void ensureActiveChunk() throws IOException {
-        if(this.activeChunk == null || !activeChunk.isWritable()){
+        if(this.activeChunk == null || activeChunk.isNotWritable()){
             // 创建新的active chunk
             this.activeChunk = new Chunk(false, chunkIdProvider.getAndIncrement());
             chunks.add(activeChunk);
@@ -166,7 +159,7 @@ public class BitCaskStorage {
     public boolean update(String key, byte[] value) throws IOException {
         synchronized (activeChunkLock){
             byte[] keyBytes = key.getBytes(OssConfigs.DEFAULT_CHARSET);
-            if(this.activeChunk == null || !activeChunk.isWritable()){
+            if(this.activeChunk == null || activeChunk.isNotWritable()){
                 // 创建新的active chunk
                 this.activeChunk = new Chunk(false, chunkIdProvider.getAndIncrement());
                 chunks.add(activeChunk);
@@ -208,30 +201,33 @@ public class BitCaskStorage {
      * 压缩、合并chunk文件
      * 1、读取所有索引，把没被删除的数据重新写到新的chunk中。
      * 2、修改index的offset和chunkId
-     * 3、把index写入HintLog
+     * 3、生成新的HintFile来记录索引
      */
     public void compact(){
         try{
             // 加锁，避免有其他线程读取
             compactLock.writeLock().lock();
-            Chunk mergedChunk = new Chunk(true, 0);
-            for (Map.Entry<String, Index> entry : indexCache.entrySet()) {
-                String key = entry.getKey();
-                Index index = entry.getValue();
-                if(!index.isRemoved() && index.getChunkId() != activeChunk.getChunkId()){
-                    byte[] keyBytes = StringUtil.getBytes(key);
-                    // 从旧的chunk读取value
-                    byte[] value = this.get(key);
-                    // 写入新chunk并更新index
-                    int offset = mergedChunk.write(keyBytes, value);
-                    index.setChunkId(0);
-                    index.setOffset(offset);
-                    // 写入Hint File
-                    hintLog.append(new HintIndex(key, index.getChunkId(), index.getOffset(), false));
+            // 只有在有其他非活跃Chunk的时候才进行compact，不对activeChunk压缩
+            if(activeChunk.getChunkId() > 0){
+                // 在compact前删除旧的Hint文件，避免compact后没有成功写入Hint文件而导致Hint和实际位置不一致的情况
+                removeOldHintFile();
+                Chunk mergedChunk = new Chunk(true, 0);
+                for (Map.Entry<String, Index> entry : indexCache.entrySet()) {
+                    String key = entry.getKey();
+                    Index index = entry.getValue();
+                    if(!index.isRemoved() && index.getChunkId() != activeChunk.getChunkId()){
+                        byte[] keyBytes = StringUtil.getBytes(key);
+                        // 从旧的chunk读取value
+                        byte[] value = this.get(key);
+                        // 写入新chunk并更新index
+                        int offset = mergedChunk.write(keyBytes, value);
+                        index.setChunkId(0);
+                        index.setOffset(offset);
+                    }
                 }
+                // 生成HintFile
+                generateHintFile();
             }
-            // 强制Hint Log刷盘
-            hintLog.swapBuffer(true);
         }catch (IOException e){
             log.warn("Compact BitCask chunk failed ", e);
         }
@@ -240,8 +236,96 @@ public class BitCaskStorage {
         }
     }
 
+    /**
+     * 删除旧的Hint文件
+     */
+    private void removeOldHintFile(){
+        String path = OssConfigs.dataPath() + File.separator + "hint.log";
+        File file = new File(path);
+        file.deleteOnExit();
+    }
+
+    /**
+     * 生成Hint 文件
+     */
+    private void generateHintFile() throws IOException {
+        ByteBuf buffer = Unpooled.buffer();
+        for (Map.Entry<String, Index> entry : indexCache.entrySet()) {
+            if(!entry.getValue().isRemoved() && entry.getValue().getChunkId() != activeChunk.getChunkId()){
+                byte[] keyBytes = StringUtil.getBytes(entry.getKey());
+                Index index = entry.getValue();
+                buffer.writeInt(keyBytes.length);
+                buffer.writeBytes(keyBytes);
+                buffer.writeInt(index.getChunkId());
+                buffer.writeInt(index.getOffset());
+            }
+        }
+        String path = OssConfigs.dataPath() + File.separator + "hint.log";
+        File file = new File(path);
+        if(!file.exists() && !file.createNewFile()){
+            throw new RuntimeException("Generate Hint File failed");
+        }
+        try(FileOutputStream outputStream = new FileOutputStream(file);
+             FileChannel channel = outputStream.getChannel()){
+            buffer.readBytes(channel, channel.size(), buffer.readableBytes());
+        }catch (IOException e){
+            log.warn("Generate Hint File Failed ", e);
+        }
+    }
+
+    /**
+     * 启动时读取Hint文件和chunk文件来加载key的索引信息
+     */
+    public void loadIndex() throws IOException {
+        String path = OssConfigs.dataPath() + File.separator + "hint.log";
+        File hintFile = new File(path);
+        // Hint文件存在，扫描Hint中的索引
+        if(hintFile.exists()){
+            parseHintFile(hintFile);
+            parseAllChunks(false);
+        }else{
+            parseAllChunks(true);
+        }
+    }
+
+    /**
+     * 加载并解析Hint文件
+     * 该情况下只解析Hint文件，不解析chunk0
+     * @param file {@link File} Hint文件
+     */
+    private void parseHintFile(File file){
+        try(FileInputStream inputStream = new FileInputStream(file);
+            FileChannel channel = inputStream.getChannel()){
+            ByteBuf buffer = Unpooled.buffer();
+            buffer.writeBytes(channel, 0, (int)channel.size());
+            while(buffer.isReadable()){
+                int keyLen = buffer.readInt();
+                byte[] keyBytes = new byte[keyLen];
+                buffer.readBytes(keyBytes);
+                int chunkId = buffer.readInt();
+                int offset = buffer.readInt();
+                Index index = new Index(chunkId, offset, false);
+                indexCache.put(StringUtil.toString(keyBytes), index);
+            }
+        }catch (IOException e){
+            log.warn("Parse Hint File Failed ", e);
+        }
+    }
+
+    /**
+     * 解析所有的chunk文件
+     */
+    private void parseAllChunks(boolean parseChunk0) throws IOException {
+        for (Chunk chunk : chunks) {
+            if(chunk.getChunkId() != 0){
+                indexCache.putAll(chunk.fullScanChunk());
+            }else if(parseChunk0){
+                indexCache.putAll(chunk.fullScanChunk());
+            }
+        }
+    }
+
     public List<String> keys(){
         return new ArrayList<>(indexCache.keySet());
     }
-
 }
