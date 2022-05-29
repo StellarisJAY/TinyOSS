cbc1fdc41a318f34c4bee282a15fdc1f4ab4a096
StellarisJAY <760409558@qq.com>
2022-05-29 14:09:32 +0800

    添加注释

--- common/src/main/java/com/jay/oss/common/remoting/BuilderStrategy.java
+++ common/src/main/java/com/jay/oss/common/remoting/BuilderStrategy.java
@@ -2,12 +2,18 @@ package com.jay.oss.common.remoting;
 
 /**
  * <p>
- *
+ *  报文Builder策略
  * </p>
  *
  * @author Jay
  * @date 2022/04/20 15:55
  */
 public interface BuilderStrategy {
+    /**
+     * 根据参数类型创建报文
+     * @param builder 报文Builder
+     * @param o 报文参数
+     * @return {@link TinyOssCommand}
+     */
     TinyOssCommand build(TinyOssCommand.TinyOssCommandBuilder builder, Object o);
 }
